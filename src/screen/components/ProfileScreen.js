0f9396d047a15160576c1f70a011144449fbe85a
Muhammad Farras Jibran <50952131+farasjibran@users.noreply.github.com>
2021-10-22 01:07:31 +0700

    Update ProfileScreen.js

--- src/screen/components/ProfileScreen.js
+++ src/screen/components/ProfileScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-shadow */
 import React, {useState, useEffect} from 'react';
